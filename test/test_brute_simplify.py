8a56a0d6cec0497276b07ff886fbea18a441c527
Nico Schlömer <nico.schloemer@gmail.com>
2017-03-01 14:16:02 +0100

    add an actual simplification test

--- test/test_brute_simplify.py
+++ test/test_brute_simplify.py
@@ -2,7 +2,7 @@ import brute_simplify
 import numpy
 
 
-def test_simple():
+def test_coefficient():
     def get_simple(x):
         e = numpy.array([x[1] - x[0], x[2] - x[0], x[3] - x[0]])
         ei_dot_ej = numpy.einsum('ilj, klj->ikl', e, e)
@@ -24,29 +24,29 @@ def test_simple():
     return
 
 
-# def test_tsps():
-#     # triple scalar product squared
-#     def get_tsps(x):
-#         e = numpy.array([x[1] - x[0], x[2] - x[0], x[3] - x[0]])
-#         return numpy.einsum('ij, ij->i', e[0], numpy.cross(e[1], e[2]))
-#
-#     out = brute_simplify.triple_tet_find(
-#         get_tsps,
-#         [[1, 0], [2, 0], [3, 0]],
-#         num_summands=5
-#         )
-#
-#     assert len(out) == 1
-#     assert (abs(out[0][0] - numpy.array([1., -1., -1.,  2., -1.]))).all()
-#     assert out[0][1] == (
-#         ((0, 0), (1, 1), (2, 2)),
-#         ((0, 0), (1, 2), (1, 2)),
-#         ((0, 1), (0, 1), (2, 2)),
-#         ((0, 1), (0, 2), (1, 2)),
-#         ((0, 2), (0, 2), (1, 1)),
-#         )
-#
-#     return
+def test_simplification():
+    def get_simple(x):
+        e = numpy.array([x[1] - x[0], x[2] - x[0], x[3] - x[0]])
+        ei_dot_ej = numpy.einsum('ilj, klj->ikl', e, e)
+        return (
+            + ei_dot_ej[0, 0] * ei_dot_ej[1, 1] * ei_dot_ej[2, 2]
+            - ei_dot_ej[0, 1] * ei_dot_ej[1, 1] * ei_dot_ej[2, 2]
+            )
+
+    out = brute_simplify.triple_tet_find(
+        get_simple,
+        [[1, 0], [2, 0], [3, 0], [2, 1], [3, 2], [1, 3]],
+        num_summands=1,
+        verbose=False
+        )
+
+    assert len(out) == 1
+    assert (abs(out[0][0] - numpy.array([-1.0])) < 1.0e-12).all()
+    assert out[0][1] == (
+        ((0, 3), (1, 1), (2, 2)),
+        )
+
+    return
 
 
 if __name__ == '__main__':
